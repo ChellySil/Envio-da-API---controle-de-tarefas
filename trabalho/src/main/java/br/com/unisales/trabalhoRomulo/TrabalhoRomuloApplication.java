a177184e208927ea583c129e74a188d3911f0f12
Michelle <109476419+ChellySil@users.noreply.github.com>
2024-03-25 19:20:44 -0300

    Add files via upload

---
+++ trabalho/src/main/java/br/com/unisales/trabalhoRomulo/TrabalhoRomuloApplication.java
@@ -0,0 +1,13 @@
+package br.com.unisales.trabalhoRomulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoRomuloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoRomuloApplication.class, args);
+	}
+
+}
